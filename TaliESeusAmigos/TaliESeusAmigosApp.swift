cb22789107d350e800f5e4fb86cca77cea269b98
Talita Santos <contato.talitansantos@gmail.com>
2022-01-12 18:37:20 -0300

    Initial Commit

---
+++ TaliESeusAmigos/TaliESeusAmigosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaliESeusAmigosApp.swift
+//  TaliESeusAmigos
+//
+//  Created by Talita Nunes dos Santos on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TaliESeusAmigosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
